44139992dabaf103736f6dbc701ef4866bf5e065
Gustavo Corrêa <156522530+gtrstx@users.noreply.github.com>
2024-10-22 01:21:08 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+function redirecionar1(){
+    window.location.href = 'pontos-turisticos.html'
+}
+
+function redirecionar2(){
+    window.location.href = 'gastronomia.html'
+}
+
+function redirecionar3(){
+    window.location.href = 'eventos.html'
+}
+
+function alerta() {
+    window.alert("Enviado com sucesso!")
+}
